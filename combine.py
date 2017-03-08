4600aea785832a7179bd359f6f8533689ea41033
Craig Kelly <craig.n.kelly@gmail.com>
2017-03-08 15:08:36 -0600

    Include daily output

--- combine.py
+++ combine.py
@@ -2,31 +2,78 @@
 
 """Extracting data from ytcrawl output dir and api.json file."""
 
-# TODO: for API and ytcrawl, we might not have 100% ID overlap, so be sure to handle missing values
-# TODO: read API Data
-# TODO: join ytcrawl with API
-# TODO: create a watch URL
-# TODO: output everything as CSV
-
 # pylama:ignore=E501,D213
 
+import collections
+import csv
+import datetime
 import json
 import os
-import tarfile
 
 from bs4 import BeautifulSoup
 
-from common import log
+from common import log, lines, rel_path
 
 # Get the location of the crawled data and the API JSON output
 import do_crawl
 import do_api
 
-CRAWL_OUTPUT = os.path.join(do_crawl.OUTPUT_DIR, "data")
+CRAWL_INPUT = os.path.join(do_crawl.OUTPUT_DIR, "data")
 API_FILE = do_api.OUTPUT_FILE
 
 # We track all brag bar labels from the crawled output for display later
 ALL_BRAG_BAR = set()
+
+# Files that we write
+OUTPUT_DATA = rel_path("output.csv")
+OUTPUT_DAILIES = rel_path("dailies.csv")
+
+DATA_COLS = [
+    "YouTubeID",
+    "WatchURL",
+    # From API "snippet"
+    "Title",
+    "Channel",
+    # From API statistics
+    "StatsCommentCount",
+    "StatsDislikeCount",
+    "StatsFavoriteCount",
+    "StatsLikeCount",
+    "StatsViewCount",
+    # From crawled brag bar
+    "BBShares",
+    "BBSubscriptions",
+    "BBViews",
+    "BBTimeWatched",
+    # From API "snippet"
+    "Description",
+    "Tags",
+]
+
+DAILY_COLS = [
+    "YouTubeID",
+    "Day",
+    "ViewCount"
+]
+
+
+def ints(s):
+    """Return a coreced int from the string unless s is empty."""
+    s = s.strip()
+    if not s:
+        return ""
+    return int(s.replace(',', ''))
+
+
+def normws(s):
+    """Return the given string with normalized whitespace."""
+    return ' '.join(s.strip().split())
+
+
+def ts_to_day(ts):
+    """Convert the JS-style timestamp to a year-month-day string."""
+    dt = datetime.datetime.utcfromtimestamp(int(ts) / 1000)
+    return dt.strftime("%Y-%m-%d")
 
 
 def extract(buffer, start_tag, end_tag):
@@ -40,15 +87,15 @@ def extract(buffer, start_tag, end_tag):
     return buffer[start+len(start_tag):end].strip()
 
 
-def process(tarinfo, reader):
-    """Extract all data in single tar file."""
-    ytid = os.path.split(tarinfo.name)[-1]
-    data = str(reader.read()).strip()
-    log("Processing: [%s] len=%12d %s", ytid, len(data), tarinfo.name)
+def process_crawled(data):
+    """Extract all data from the single file contents.
+
+    Return is tuple of 2 dictionaries: (daily_stats, brag_bar)
+    """
     err_msg = extract(data, "<error_message><![CDATA[", "]]></error_message>")
     if err_msg:
         log("  Skipping: %s", err_msg)
-        return False
+        return None, None
 
     daily_stats = json.loads(extract(data, "<graph_data><![CDATA[", "]]></graph_data>"))
     brag_bar_html = extract(data, "<html_content><![CDATA[", "]]></html_content>")
@@ -70,32 +117,99 @@ def process(tarinfo, reader):
             brag_bar[label] = value
             ALL_BRAG_BAR.add(label)
 
-    daily_stats["YTID"] = ytid
-    daily_stats["BragBar"] = brag_bar
-    print(json.dumps(daily_stats))
-
-    return True
+    return daily_stats, brag_bar
 
 
 def main():
     """Entry point."""
-    skip_names = set(["key.done", "log"])
+    # We store everything as a dict keyed on YouTube ID
+    all_data = collections.defaultdict(dict)
 
+    # Process crawled data
+    log("Processing crawled data")
     count, processed = 0, 0
-
-    with tarfile.open("ytcrawl.tar.gz") as tf:
-        for tarinfo in tf:
-            if not tarinfo.isfile() or tarinfo.size < 1:
-                continue
-            if tarinfo.name in skip_names or "data/" not in tarinfo.name:
-                continue
-
+    for path, subdirs, files in os.walk(CRAWL_INPUT):
+        for fn in files:
             count += 1
-            if process(tarinfo, tf.extractfile(tarinfo)):
-                processed += 1
+            # Remember, file name is also the youtube key in the crawl output
+            with open(os.path.join(path, fn), "r") as fh:
+                data = fh.read().strip()
+            log("Processing crawled data: [%s] len=%12d", fn, len(data))
+            daily_stats, brag_bar = process_crawled(data)
+            if not daily_stats and not brag_bar:
+                continue  # Nothing to do
 
-    log("Processed %d out of %d", processed, count)
+            processed += 1
+            all_data[fn]["daily_stats"] = daily_stats
+            all_data[fn]["brag_bar"] = brag_bar
+
+    log("Processed crawled data: %d out of %d", processed, count)
     log("All brag bar labels: %s", repr(ALL_BRAG_BAR))
+
+    # Process API content
+    log("Processing JSON retrieve from API")
+    count, processed = 0, 0
+    for line in lines(API_FILE):
+        count += 1
+        rec = json.loads(line)
+        ytid = rec.get("id", "").strip()
+        if not ytid:
+            continue
+
+        processed += 1
+        all_data[ytid]["stats"] = rec.get("statistics", {})
+        all_data[ytid]["snippet"] = rec.get("snippet", {})
+
+    log("Processed API data: %d out of %d", processed, count)
+
+    # Output final CSV files
+    log("Writing output file:     %s", OUTPUT_DATA)
+    log("Writing daily data file: %s", OUTPUT_DAILIES)
+    data_count, daily_count = 0, 0
+    with open(OUTPUT_DATA, "w") as data_fh, open(OUTPUT_DAILIES, "w") as daily_fh:
+        data_csv = csv.writer(data_fh, quoting=csv.QUOTE_NONNUMERIC)
+        data_csv.writerow(DATA_COLS)
+
+        daily_csv = csv.writer(daily_fh, quoting=csv.QUOTE_NONNUMERIC)
+        daily_csv.writerow(DAILY_COLS)
+
+        for ytid, data in all_data.items():
+            # From API
+            stats = data.get("stats", {})
+            snippet = data.get("snippet", {})
+            # From crawl
+            daily_stats = data.get("daily_stats", {})
+            brag_bar = data.get("brag_bar", {})
+
+            # Write out the main data record
+            data_count += 1
+            data_csv.writerow([
+                ytid,                                            # YouTubeID
+                "https://youtube.com/watch?v="+ytid,             # WatchURL
+                normws(snippet.get("title", "")),                # Title
+                snippet.get("channelTitle", ""),                 # Channel
+                ints(stats.get("commentCount", "")),             # StatsCommentCount
+                ints(stats.get("dislikeCount", "")),             # StatsDislikeCount
+                ints(stats.get("favoriteCount", "")),            # StatsFavoriteCount
+                ints(stats.get("likeCount", "")),                # StatsLikeCount
+                ints(stats.get("viewCount", "")),                # StatsViewCount
+                ints(brag_bar.get("Shares", "")),                # BBShares
+                ints(brag_bar.get("Subscriptions driven", "")),  # BBSubscriptions
+                ints(brag_bar.get("Views", "")),                 # BBViews
+                ints(brag_bar.get("Time watched", "")),          # BBTimeWatched
+                normws(snippet.get("description", "")),          # Description
+                "|".join(snippet.get("tags", [])),               # Tags
+            ])
+
+            # Write out all the daily views
+            days = daily_stats.get("day", {}).get("data", [])
+            views = daily_stats.get("views", {}).get("daily", {}).get("data", [])
+            for d, v in zip(days, views):
+                daily_csv.writerow([ytid, ts_to_day(d), v])
+                daily_count += 1
+
+    log("Wrote %12d to %s", data_count, OUTPUT_DATA)
+    log("Wrote %12d to %s", daily_count, OUTPUT_DAILIES)
 
 
 if __name__ == '__main__':
